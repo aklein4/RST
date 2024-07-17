f4c1e0e3f2a8da26194af2662e9986d1ca8a058c
Adam Klein <adam3207@gmail.com>
2024-07-16 22:46:14 -0500

    ready 2 go

--- src/test_rat.py
+++ src/test_rat.py
@@ -1,22 +1,46 @@
 import torch
 
+import numpy as np
+
 from transformers import AutoTokenizer
 
 from models.rat import RatConfig, RatLmModel
+from models.base import BaseConfig, BaseLmModel
 from utils.config_utils import load_model_config
 import utils.constants as constants
 
 
-MODEL_CONFIG = 'test-rat'
+MODEL_CONFIG = 'long-rat'
+BASE_CONFIG = 'long-base'
+
+COMPARE = True
 
 
 def main():
 
-
     print("Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(constants.GPT2_TOKENIZER, resume_download=None)
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+
+    if COMPARE:
+
+        print("loading model...")
+        config = load_model_config(MODEL_CONFIG, tokenizer)
+        model = RatLmModel(RatConfig(**config), fast_start=True)
+
+        print("loading base...")
+        base_config = load_model_config(BASE_CONFIG, tokenizer)
+        base_model = BaseLmModel(BaseConfig(**base_config), fast_start=True)
     
+        n = np.sum([p.numel() for p in model.parameters()])
+        n_base = np.sum([p.numel() for p in base_model.parameters()])
+
+        print(f"Rat: {n:_} parameters")
+        print(f"Base: {n_base:_} parameters")
+        print(f"Diff: {n - n_base:_}")
+
+        return
+
     x = tokenizer(["Hello, my dog is cute", "His dog is cute too", "All dogs are cute"], return_tensors="pt", padding="max_length", max_length=16).input_ids
 
     print("loading model...")
