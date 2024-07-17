d7872d8ee8408460e3617e66bd8501a2533e9f9a
Adam Klein <adam3207@gmail.com>
2024-07-16 22:26:29 -0500

    deprecated idr

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,17 +1,14 @@
 """ Models """
 
 from models.base import BaseConfig, BaseLmModel
-from models.idr import IDRConfig, IDRLmModel
 from models.rat import RatConfig, RatLmModel
 
 CONFIG_DICT = {
     "base": BaseConfig,
-    "idr": IDRConfig,
     "rat": RatConfig,
 }
 
 MODEL_DICT = {
     "base": BaseLmModel,
-    "idr": IDRLmModel,
     "rat": RatLmModel,
 }
